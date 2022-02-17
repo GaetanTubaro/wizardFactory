60b490ecd1d4512a02357650ab76b26da087bf49
Clara <clara.louise.goncalves@gmail.com>
2022-02-17 14:13:38 +0100

    6 - ajout de la modification équipement

--- pages/details.php
+++ pages/details.php
@@ -44,65 +44,88 @@ if (isset($_GET['type'])) {
                             <p class="mb-0">Oups ! <?= $error ?> Veuillez entrer une donnée valide.</p>
                             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                         </div><?php
-                    }
+                            }
                 }
             }
-            break;
-        case 'changeSkill':
-            if (isset($_POST['skill_name']) && isAString($_POST['skill_name']) && isset($_POST['skill_level']) && is_numeric($_POST['skill_level']) && isset($_POST['skill_stat'])) {
-                $skillChange = new Skill();
-                $skillChange
-                    ->setId($_GET['idSkill'])
-                    ->setName($_POST['skill_name'])
-                    ->setLevel($_POST['skill_level'])
-                    ->setStats($_POST['skill_stat'])
-                    ->setOwner($id_character);
-                $errors = $skillChange->checkData();
-                if (empty($errors)) {
-                    $changeSkill = $connection->prepare('UPDATE skills SET name = "' . $skillChange->getName() . '", level = ' . $skillChange->getLevel() . ', stats = "' . $skillChange->getStats() . '" WHERE id =' . $skillChange->getId());
-                    $changeSkill->execute();
-                    header('Location: ?page=details&character=' . $id_character);
-                } else {
-                    foreach ($errors as $error) { ?>
+                    break;
+                case 'changeSkill':
+                    if (isset($_POST['skill_name']) && isAString($_POST['skill_name']) && isset($_POST['skill_level']) && is_numeric($_POST['skill_level']) && isset($_POST['skill_stat'])) {
+                        $skillChange = new Skill();
+                        $skillChange
+                            ->setId($_GET['idSkill'])
+                            ->setName($_POST['skill_name'])
+                            ->setLevel($_POST['skill_level'])
+                            ->setStats($_POST['skill_stat'])
+                            ->setOwner($id_character);
+                        $errors = $skillChange->checkData();
+                        if (empty($errors)) {
+                            $changeSkill = $connection->prepare('UPDATE skills SET name = "' . $skillChange->getName() . '", level = ' . $skillChange->getLevel() . ', stats = "' . $skillChange->getStats() . '" WHERE id =' . $skillChange->getId());
+                            $changeSkill->execute();
+                            header('Location: ?page=details&character=' . $id_character);
+                        } else {
+                            foreach ($errors as $error) { ?>
                         <div class="alert alert-warning alert-dismissible fade show w-50 mx-auto my-3" role="alert">
                             <p class="mb-0">Oups ! <?= $error ?> Veuillez entrer une donnée valide.</p>
                             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                         </div><?php
+                            }
+                        }
                     }
-                }
-            }
-            break;
-        case 'deleteSkill':
-            $deleteSkill = $connection->prepare('DELETE FROM skills WHERE id=' . $_GET['idSkill']);
-            $deleteSkill->execute();
-            header('Location: ?page=details&character=' . $id_character);
-            break;
-        case 'addEquipment':
-            if (isset($_POST['equipment_name']) && isAString($_POST['equipment_name']) && isset($_POST['equipment_range']) && is_numeric($_POST['equipment_range']) && isset($_POST['equipment_damages'])  && is_numeric($_POST['equipment_damages'])) {
-                $equipmentAdded = new Equipment();
-                $equipmentAdded
-                    ->setName($_POST['equipment_name'])
-                    ->setDamages($_POST['equipment_damages'])
-                    ->setRange($_POST['equipment_range'])
-                    ->setOwner($id_character);
-                $errors = $equipmentAdded->checkData();
-                if (empty($errors)) {
-                    $addEquipment = $connection->prepare('INSERT INTO equipments (name, damages, range_area, id_charac) VALUES ("' . $equipmentAdded->getName() . '","' . $equipmentAdded->getDamages() . '",' . $equipmentAdded->getRange() . ',' . $equipmentAdded->getOwner() . ')');
-                    $addEquipment->execute();
+                    break;
+                case 'deleteSkill':
+                    $deleteSkill = $connection->prepare('DELETE FROM skills WHERE id=' . $_GET['idSkill']);
+                    $deleteSkill->execute();
                     header('Location: ?page=details&character=' . $id_character);
-                } else {
-                    foreach ($errors as $error) {?>
+                    break;
+                case 'addEquipment':
+                    if (isset($_POST['equipment_name']) && isAString($_POST['equipment_name']) && isset($_POST['equipment_range']) && is_numeric($_POST['equipment_range']) && isset($_POST['equipment_damages'])  && is_numeric($_POST['equipment_damages'])) {
+                        $equipmentAdded = new Equipment();
+                        $equipmentAdded
+                            ->setName($_POST['equipment_name'])
+                            ->setDamages($_POST['equipment_damages'])
+                            ->setRange($_POST['equipment_range'])
+                            ->setOwner($id_character);
+                        $errors = $equipmentAdded->checkData();
+                        if (empty($errors)) {
+                            $addEquipment = $connection->prepare('INSERT INTO equipments (name, damages, range_area, id_charac) VALUES ("' . $equipmentAdded->getName() . '","' . $equipmentAdded->getDamages() . '",' . $equipmentAdded->getRange() . ',' . $equipmentAdded->getOwner() . ')');
+                            $addEquipment->execute();
+                            header('Location: ?page=details&character=' . $id_character);
+                        } else {
+                            foreach ($errors as $error) { ?>
                         <div class="alert alert-warning alert-dismissible fade show w-50 mx-auto my-3" role="alert">
                             <p class="mb-0">Oups ! <?= $error ?> Veuillez entrer une donnée valide.</p>
                             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                         </div><?php
+                            }
+                        }
                     }
-                }
+                    break;
+                case 'changeEquipment':
+                    if (isset($_POST['equipment_name']) && isAString($_POST['equipment_name']) && isset($_POST['equipment_range']) && is_numeric($_POST['equipment_range']) && isset($_POST['equipment_damages'])  && is_numeric($_POST['equipment_damages'])) {
+                        $equipmentChange = new Equipment();
+                        $equipmentChange
+                            ->setName($_POST['equipment_name'])
+                            ->setDamages($_POST['equipment_damages'])
+                            ->setRange($_POST['equipment_range'])
+                            ->setOwner($id_character);
+                        $errors = $equipmentChange->checkData();
+                        if (empty($errors)) {
+                            $changeEquipment = $connection->prepare('UPDATE equipments SET name ="' . $equipmentChange->getName() . '", damages =' . $equipmentChange->getDamages() . ', range_area =' . $equipmentChange->getRange() . ' WHERE id=' . $_GET['idEquipment']);
+                            $changeEquipment->execute();
+                            header('Location: ?page=details&character=' . $id_character);
+                        } else {
+                            foreach ($errors as $error) { ?>
+                        <div class="alert alert-warning alert-dismissible fade show w-50 mx-auto my-3" role="alert">
+                            <p class="mb-0">Oups ! <?= $error ?> Veuillez entrer une donnée valide.</p>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div><?php
+                            }
+                        }
+                    }
+                    break;
             }
-            break;
-    }
 }
-?>
+                                ?>
 
 <link href="css/details.css" rel="stylesheet">
 
@@ -152,12 +175,12 @@ if (isset($_GET['type'])) {
                                         <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
                                     </svg>
                                 </button>
-                                <a href="?page=details&character=<?= $_GET['character']?>&type=deleteSkill&idSkill=<?= $skill->getId()?>"><button class="btn m-1 p-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-                                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
-                                    </svg>
-                                </button></a>
+                                <a href="?page=details&character=<?= $_GET['character'] ?>&type=deleteSkill&idSkill=<?= $skill->getId() ?>"><button class="btn m-1 p-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                                        </svg>
+                                    </button></a>
                             </span>
                         </div>
                         <p class="card-text mb-1"><span style='font-style:bold'>Statistique liée :</span> <?= $skill->getStats() ?></p>
@@ -186,8 +209,8 @@ if (isset($_GET['type'])) {
                                         <select class="form-select" name="skill_stat">
                                             <?php foreach (Skill::POSSIBLE_STATS as $statistique) { ?>
                                                 <option value="<?= $statistique ?>" <?php if ($skill->getStats() == $statistique) {
-    echo "selected";
-} ?>><?= $statistique ?>
+                                    echo "selected";
+                                } ?>><?= $statistique ?>
                                                 </option>
                                             <?php } ?>
                                         </select>
@@ -207,7 +230,7 @@ if (isset($_GET['type'])) {
 
             <?php } ?>
             <button class="btn btn-primary ms-auto" data-bs-toggle="modal" data-bs-target="#skillAddForm">Créer une nouvelle compétence</button>
-            
+
             <!---------------------------------------------------------->
             <!-------------- Modal de création compétence -------------->
             <!---------------------------------------------------------->
@@ -247,7 +270,7 @@ if (isset($_GET['type'])) {
                 </div>
             </div>
             <!-- Fin Modal de création compétence -->
-        
+
         </div>
         <div class="col-4 d-flex flex-column">
             <h2>Equipements</h2>
@@ -257,27 +280,61 @@ if (isset($_GET['type'])) {
                         <div class="card-title d-flex align-items-center">
                             <h5 class="m-0 d-inline"><?= $equipment->getName() ?></h5>
                             <span class="ms-auto">
-                                <button class="btn m-1 p-1" data-bs-toggle="modal" data-bs-target="#EquipmentChangeForm<?= $equipment->getId() ?>">
+                                <button class="btn m-1 p-1" data-bs-toggle="modal" data-bs-target="#equipmentChangeForm<?= $equipment->getId() ?>">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
                                         <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
                                     </svg>
                                 </button>
-                                <a href="?page=details&character=<?= $_GET['character']?>&type=deleteEquipment&idEquipment=<?= $equipment->getId()?>"><button class="btn m-1 p-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-                                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
-                                    </svg>
-                                </button></a>
+                                <a href="?page=details&character=<?= $_GET['character'] ?>&type=deleteEquipment&idEquipment=<?= $equipment->getId() ?>"><button class="btn m-1 p-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                                        </svg>
+                                    </button></a>
                             </span>
                         </div>
                         <p class="card-text mb-1"><span style='font-style:bold'>Dégâts :</span> <?= $equipment->getDamages() ?></p>
                         <p class="card-text mb-1"><span style='font-style:bold'>Portée :</span> <?= $equipment->getRange() ?></p>
                     </div>
                 </div>
+
+                <!---------------------------------------------------------->
+                <!----------------- Modal modif equipement ----------------->
+                <!---------------------------------------------------------->
+                <div class="modal fade" id="equipmentChangeForm<?= $equipment->getId() ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="staticBackdropLabel">Modifier un équipement</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <form name="changeEquipment" action="" method="POST">
+                                    <div class="mb-3">
+                                        <label class="form-label">Nom</label>
+                                        <input type="text" class="form-control" name="equipment_name" value="<?= $equipment->getName() ?>" required>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label class="form-label">Dégâts</label>
+                                        <span class="form-text mt-0 ms-3">Supérieur à 0</span>
+                                        <input type="number" class="form-control" name="equipment_damages" value="<?= $equipment->getDamages() ?>" required>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label class="form-label">Portée</label>
+                                        <span class="form-text mt-0 ms-3">Comprise entre 0 et 5</span>
+                                        <input type="number" class="form-control" name="equipment_range" value="<?= $equipment->getRange() ?>" required>
+                                    </div>
+                                    <button formaction="?page=details&character=<?= $id_character ?>&type=changeEquipment&idEquipment=<?= $equipment->getId() ?>" type="submit" class="btn btn-primary">Modifier</button></formaction=>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Fin modal modif compétence -->
             <?php } ?>
             <button class="btn btn-primary ms-auto" data-bs-toggle="modal" data-bs-target="#equipmentAddForm">Créer un nouvel équipement</button>
-            
+
             <!---------------------------------------------------------->
             <!-------------- Modal de création équipements ------------->
             <!---------------------------------------------------------->
@@ -295,7 +352,7 @@ if (isset($_GET['type'])) {
                                     <input type="text" class="form-control" name="equipment_name" required>
                                 </div>
                                 <div class="mb-3">
-                                <label class="form-label">Dégâts</label>
+                                    <label class="form-label">Dégâts</label>
                                     <span class="form-text mt-0 ms-3">Supérieur à 0</span>
                                     <input type="number" class="form-control" name="equipment_damages" required>
                                 </div>
