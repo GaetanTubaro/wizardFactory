5b8386da52277bdb03d23eb4b8e309994dade036
Clara <clara.louise.goncalves@gmail.com>
2022-02-18 13:47:27 +0100

    Refonte liste

--- pages/list.php
+++ pages/list.php
@@ -8,9 +8,7 @@ $searchCharacters->execute();
 $characters = $searchCharacters->fetchAll(PDO::FETCH_CLASS, Character::class);
 ?>
 
-<a href="?page=creationCharForm"><button class="btn btn-primary">Créer un nouveau personnage</button></a>
-
-<div class="liste mx-2">
+<div class="liste mt-4 mx-2 d-flex flex-wrap align-items-stretch">
     <?php foreach ($characters as $character) { ?>
 
         <div class="card mx-2" style="width: 15rem;">
@@ -35,4 +33,9 @@ $characters = $searchCharacters->fetchAll(PDO::FETCH_CLASS, Character::class);
     <?php
     }
     ?>
+    <a href="?page=creationCharForm"><div class="card card-add mx-2 h-100" style="width:15rem">
+        <svg xmlns="http://www.w3.org/2000/svg" width="50%" height="auto" fill="currentColor" class="bi bi-plus-lg mx-auto" viewBox="0 0 16 16">
+        <path d="M8 6.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 .5-.5z"/>
+  <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z"/></svg>
+    </div></a>
 </div>
