aa086c28fe4c1cc0b16d47f1eab7b44f48c81ab2
Clara <clara.louise.goncalves@gmail.com>
2022-02-18 15:07:33 +0100

    encore et toujours plus de css

--- pages/list.php
+++ pages/list.php
@@ -12,13 +12,18 @@ $characters = $searchCharacters->fetchAll(PDO::FETCH_CLASS, Character::class);
     <?php foreach ($characters as $character) { ?>
 
         <div class="card mx-2" style="width: 15rem;">
-            <a href="?page=details&character=<?= $character->getId() ?>">
-                <div class="d-flex flex-column align-items-center" style="height:15rem; width:100%;box-sizing:border-box"><img src="<?= $character->getImg() ?>" class="card-img-top" style="max-height:15rem; max-width:100%;width:auto;height:auto;box-sizing:border-box"></div>
-                <div class="card-body">
+
+            <div class="d-flex flex-column align-items-center" style="height:15rem; width:100%;box-sizing:border-box">
+                <a href="?page=details&character=<?= $character->getId() ?>"><img src="<?= $character->getImg() ?>" class="card-img-top" style="max-height:15rem; max-width:100%;width:auto;height:auto;box-sizing:border-box">
+            </div>
+
+            </a>
+            <div class="card-body d-flex justify-content-center align-items-center">
+                <a href="?page=details&character=<?= $character->getId() ?>">
                     <h2 class="card-title text-center"><?= $character->getName() ?>
                     </h2>
-                </div>
-            </a>
+                </a>
+            </div>
             <div class="card-footer d-flex justify-content-center">
                 <a href="?page=details&character=<?= $character->getId() ?>&type=deleteChar" class="w-100">
                     <button class="btn m-0 p-0 w-100">
@@ -33,9 +38,12 @@ $characters = $searchCharacters->fetchAll(PDO::FETCH_CLASS, Character::class);
     <?php
     }
     ?>
-    <a href="?page=creationCharForm"><div class="card card-add mx-2 h-100" style="width:15rem; min-height:15rem">
-        <svg xmlns="http://www.w3.org/2000/svg" width="50%" height="auto" fill="currentColor" class="bi bi-plus-lg m-auto" viewBox="0 0 16 16">
-        <path d="M8 6.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 .5-.5z"/>
-  <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z"/></svg>
-    </div></a>
+    <a href="?page=creationCharForm">
+        <div class="card card-add mx-2 h-100" style="width:15rem; min-height:15rem">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50%" height="auto" fill="currentColor" class="bi bi-plus-lg m-auto" viewBox="0 0 16 16">
+                <path d="M8 6.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 .5-.5z" />
+                <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z" />
+            </svg>
+        </div>
+    </a>
 </div>
