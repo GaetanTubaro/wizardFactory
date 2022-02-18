c07147b7ca43b9a4f4a79d67bea20f46efefdd4d
Clara <clara.louise.goncalves@gmail.com>
2022-02-18 12:18:06 +0100

    7 - bouton supprimer sur la liste et sur les détails du personnage

--- pages/details.php
+++ pages/details.php
@@ -45,8 +45,8 @@ if (isset($_GET['type'])) {
                             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                         </div><?php
                             }
-                        }
-                    }
+                }
+            }
                     break;
                 case 'changeSkill':
                     if (isset($_POST['skill_name']) && isAString($_POST['skill_name']) && isset($_POST['skill_level']) && is_numeric($_POST['skill_level']) && isset($_POST['skill_stat'])) {
@@ -147,8 +147,13 @@ if (isset($_GET['type'])) {
                         }
                     }
                     break;
+                case 'deleteChar':
+                    $deleteChar = $connection->prepare('DELETE FROM character_sheets WHERE id=' . $_GET['character']);
+                    $deleteChar->execute();
+                    header('Location: ?page=list');
+                    break;
             }
-        }
+}
                                 ?>
 
 <link href="css/details.css" rel="stylesheet">
@@ -168,12 +173,12 @@ if (isset($_GET['type'])) {
                             <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
                         </svg>
                     </button>
-                    <button class="btn m-1 p-1">
+                    <a href="?page=details&character=<?= $character->getId() ?>&type=deleteChar"><button class="btn m-1 p-1">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
                             <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
                         </svg>
-                    </button>
+                    </button></a>
                 </span>
             </div>
             <div class="row">
@@ -363,8 +368,8 @@ if (isset($_GET['type'])) {
                                         <select class="form-select" name="skill_stat">
                                             <?php foreach (Skill::POSSIBLE_STATS as $statistique) { ?>
                                                 <option value="<?= $statistique ?>" <?php if ($skill->getStats() == $statistique) {
-                                                                                        echo "selected";
-                                                                                    } ?>><?= $statistique ?>
+                                    echo "selected";
+                                } ?>><?= $statistique ?>
                                                 </option>
                                             <?php } ?>
                                         </select>
