2bb8d3e42c38f5c2385f775d1c867bedb31f9fda
Clara <clara.louise.goncalves@gmail.com>
2022-02-21 12:15:30 +0100

    Ajout classe game

---
+++ classes/Game.php
@@ -0,0 +1,68 @@
+<?php
+
+class Game
+{
+    protected int $id;
+    protected string $name;
+    protected int $id_mj;
+
+    /**
+     * Get the value of name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of id_mj
+     */
+    public function getId_mj()
+    {
+        return $this->id_mj;
+    }
+
+    /**
+     * Set the value of id_mj
+     *
+     * @return  self
+     */
+    public function setId_mj($id_mj)
+    {
+        $this->id_mj = $id_mj;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+}
