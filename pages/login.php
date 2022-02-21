4f6be8b84d13e6b71a80816a64aada1d181ca1d3
Gaetan_Tubaro <tubaro.gaetan@gmail.com>
2022-02-21 11:59:51 +0100

    changement des alertes des pages login et sign up

--- pages/login.php
+++ pages/login.php
@@ -1,8 +1,24 @@
 <?php if (isset($_SESSION["login"])) {
     header('Location: ?page=list');
 }
+if (isset($_POST['pseudo'])) {
+    $statement = $connection->query("SELECT * FROM users WHERE pseudo = '" . $_POST['pseudo'] . "'");
+    $statement->setFetchMode(PDO::FETCH_CLASS, "Users");
+    $user = $statement->fetch();
+    if ($user != false && $user->getPassword() == $_POST['pass']) {
+        echo 'Connectated';
+        $_SESSION["login"] = $user->getPseudo();
+        $_SESSION["id"] = $user->getId();
+        header('Location:?page=list');
+    } else { ?>
+        <div class="alert alert-warning alert-dismissible fade show w-50 mx-auto my-3" role="alert">
+            <p class="mb-0"> Identifiants incorrects</p>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div><?php
+            }
+        }
+                ?>
 
-?>
 <form class="d-flex flex-column justify-content-center w-25 mx-auto p-5 " method="POST">
     <div class="mb-3">
         <label for="inputId" class="form-label">Pseudo</label>
@@ -14,21 +30,3 @@
     </div>
     <button type="submit" class="btn btn-primary">Se connecter</button>
 </form>
-
-<?php
-if (isset($_POST['pseudo'])) {
-    $statement = $connection->query("SELECT * FROM users WHERE pseudo = '".$_POST['pseudo']."'");
-    $statement->setFetchMode(PDO::FETCH_CLASS, "Users");
-    $user = $statement->fetch();
-    if ($user->getPassword() == $_POST['pass']) {
-        echo 'Connectated';
-        $_SESSION["login"] = $user->getPseudo();
-        $_SESSION["id"] = $user->getId();
-        header('Location:?page=list');
-    } else {
-        echo "Identifiants Incorrects";
-    }
-}
-// if (isset($_POST["pass"]) && $_POST["pass"] != $myPass) {
-//     echo 'Mauvais mot de passe';
-// }
--- pages/signup.php
+++ pages/signup.php
@@ -1,4 +1,29 @@
-<form class="d-flex flex-column justify-content-center w-25 mx-auto p-5 " method="POST">
+<?php
+
+if (isset($_POST['pseudo']) && $_POST['pass'] && $_POST['verifPass']) {
+    if ($_POST['pass'] != $_POST['verifPass']) {
+        echo "Le mot de passe n'est pas verifié";
+    } else {
+        $signup = $connection->query("SELECT COUNT(*) AS count FROM users WHERE pseudo ='" . $_POST['pseudo'] . "'");
+        $signup = $signup->fetchAll(PDO::FETCH_ASSOC);
+        if ($signup[0]['count'] != '0') { ?>
+            <div class="alert alert-warning alert-dismissible fade show w-50 mx-auto my-3" role="alert">
+                <p class="mb-0">Le pseudo déjà existant.</p>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div><?php
+                } else {
+                    $statement = $connection->query("INSERT INTO users(pseudo,password) VALUES ('" . $_POST['pseudo'] . "','" . $_POST['pass'] . "')"); ?>
+            <div class="alert alert-primary alert-dismissible fade show w-50 mx-auto my-3" role="alert">
+                <p class="mb-0">Votre compte a été créé.</p>
+                <p class="mb-0"><a href="?page=login">Cliquez pour vous connecter.</a></p>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div><?php
+                }
+            }
+        }
+                    ?>
+
+<form class="d-flex flex-column justify-content-center w-25 mx-auto pt-4" method="POST">
     <div class="mb-3">
         <label for="inputId" class="form-label">Pseudo</label>
         <input type="text" class="form-control" name="pseudo" required>
@@ -13,21 +38,3 @@
     </div>
     <button type="submit" class="btn btn-primary">S'inscrire</button>
 </form>
-
-<?php
-
-if (isset($_POST['pseudo']) && $_POST['pass'] && $_POST['verifPass']) {
-    if ($_POST['pass'] != $_POST['verifPass']) {
-        echo "Le mot de passe n'est pas verifié";
-    } else {
-        $signup = $connection->query("SELECT COUNT(*) AS count FROM users WHERE pseudo ='".$_POST['pseudo']."'");
-        $signup = $signup->fetchAll(PDO::FETCH_ASSOC);
-        if ($signup[0]['count']!='0') {
-            echo 'Le pseudo existe déjà';
-        } else {
-            $statement = $connection->query("INSERT INTO users(pseudo,password) VALUES ('".$_POST['pseudo']."','".$_POST['pass']."')"); ?>
-<a href="?page=login">Le compte à été crée. Connectez-vous.</a>;
-<?php
-        }
-    }
-}
