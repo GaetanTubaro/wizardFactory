1bafc7d3c241a5fb4bff676b6b9fb69b19c453bd
Gaetan_Tubaro <tubaro.gaetan@gmail.com>
2022-02-21 14:29:39 +0100

    maj navbar

--- includes/header.php
+++ includes/header.php
@@ -14,10 +14,8 @@ require_once 'includes/functions.php';
 <html lang="en">
 
 <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -29,23 +27,21 @@ require_once 'includes/functions.php';
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Wizard Factory</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                     <?php
-                        if (isset($_SESSION["login"])) {
-                            echo '<a class="nav-link" href="?page=list">Liste</a>';
-                            echo '<a class="nav-link" href="?page=creationCharForm">Créer</a>';
-                            echo '<a class="nav-link" href="?page=logout">Deconnexion</a>';
-                            echo '<a class="nav-link"> Bienvenue ' . $_SESSION["login"] . '!! :D</a>';
-                        } else {
-                            echo '<a class="nav-link" href="?page=login">Connexion</a>';
-                            echo '<a class="nav-link" href="?page=signup">Inscription</a>';
-                        }
-                            ?>
+                    if (isset($_SESSION["login"])) {
+                        echo '<a class="nav-link" href="?page=list">Tables</a>';
+                        echo '<a class="nav-link" href="?page=logout">Deconnexion</a>';
+                        echo '<a class="nav-link"> Bienvenue ' . $_SESSION["login"] . '!! :D</a>';
+                    } else {
+                        echo '<a class="nav-link" href="?page=login">Connexion</a>';
+                        echo '<a class="nav-link" href="?page=signup">Inscription</a>';
+                    }
+                    ?>
                 </div>
             </div>
         </div>
