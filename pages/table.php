f82f74e034178e1f03829a7a5cbc32cf80a323bb
Yannis BOUGOURZI <kaiva.kono@gmail.com>
2022-02-24 14:08:58 +0100

    Tentatives

--- pages/table.php
+++ pages/table.php
@@ -29,6 +29,45 @@ if (isset($_GET['type'])) {
         }
     }
 }
+$equipTable = $connection->prepare('SELECT * FROM `equipment` WHERE id_charac IS NULL');
+$equipTable->setFetchMode(PDO::FETCH_CLASS, Equipment::class);
+$unequiped = $equipTable->fetchAll();
+
+    foreach ($unequiped as $unequip) {
+        ?>
+
+<div class="card mx-2" style="width: 15rem;">
+
+    <div class="d-flex flex-column align-items-center" style="height:15rem; width:100%;box-sizing:border-box">
+        <a href="#" class="card-img-top"
+            style="max-height:15rem; max-width:100%;width:auto;height:auto;box-sizing:border-box">
+    </div>
+
+    </a>
+    <div class="card-body d-flex justify-content-center align-items-center">
+        <h2 class="card-title text-center"><?= $unequiped->getName() ?>
+        </h2>
+        </a>
+    </div>
+    <div class="card-footer d-flex justify-content-center">
+        <a href="?page=details&character=<?= $character->getId() ?>&type=deleteChar"
+            class="w-100">
+            <button class="btn m-0 p-0 w-100">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash"
+                    viewBox="0 0 16 16">
+                    <path
+                        d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                    <path fill-rule="evenodd"
+                        d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                </svg>
+            </button>
+        </a>
+    </div>
+</div>
+
+<?php
+    }
+
 ?>
 
 <h1 class="mx-3 pt-3">Personnages de la partie : <?= $game->getName() ?>
@@ -105,45 +144,61 @@ if (isset($_GET['type'])) {
 </div>
 
 <?php
+
+
 if (isset($_POST['equipment_name']) && isAString($_POST['equipment_name']) && isset($_POST['equipment_range']) && is_numeric($_POST['equipment_range']) && isset($_POST['equipment_damages'])  && is_numeric($_POST['equipment_damages'])) {
-        $infoEquip = array_merge($_POST);
-        $equipmentAdded = new Equipment($infoEquip);
-        $equipmentAdded->addEquipment($connection);
+    $infoEquip = array_merge($_POST, ['id']);
+    $equipmentAdded = new Equipment($infoEquip);
+    $equipmentAdded->addEquipment($connection);
+    $id_charac=$equipmentAdded->getOwner(); ?>
+<div class="alert alert-primary alert-dismissible fade show w-50 mx-auto my-3" role="alert">
+    <p class="mb-0">Votre compte a été créé.</p>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+<?php
+} else {
+        ?>
+<div class="alert alert-warning alert-dismissible fade show w-50 mx-auto my-3" role="alert">
+    <p class="mb-0">La création d'équipement à echoué</p>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    <?php
     }
 ?>
 
-<button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#equipmentAddForm"> Créer un nouvel équipement
-</button>
 
-<div class="modal fade" id="equipmentAddForm" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-    aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="staticBackdropLabel">Ajouter un équipement</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form name="addEquipment" action="" method="POST">
-                    <div class="mb-3">
-                        <label class="form-label">Nom</label>
-                        <input type="text" class="form-control" name="equipment_name" required>
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">Dégâts</label>
-                        <span class="form-text mt-0 ms-3">Supérieur à 0</span>
-                        <input type="number" class="form-control" name="equipment_damages" required>
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">Portée</label>
-                        <span class="form-text mt-0 ms-3">Comprise entre 0 et 5</span>
-                        <input type="number" class="form-control" name="equipment_range" required>
-                    </div>
-                    <button
-                        formaction="?page=table&table=<?= $id_game ?>&type=addEquipment"
-                        type="submit" class="btn btn-primary">Ajouter</button>
-                </form>
+    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#equipmentAddForm"> Créer un nouvel
+        équipement
+    </button>
+
+    <div class="modal fade" id="equipmentAddForm" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Ajouter un équipement</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form name="addEquipment" action="" method="POST">
+                        <div class="mb-3">
+                            <label class="form-label">Nom</label>
+                            <input type="text" class="form-control" name="equipment_name" required>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Dégâts</label>
+                            <span class="form-text mt-0 ms-3">Supérieur à 0</span>
+                            <input type="number" class="form-control" name="equipment_damages" required>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Portée</label>
+                            <span class="form-text mt-0 ms-3">Comprise entre 0 et 5</span>
+                            <input type="number" class="form-control" name="equipment_range" required>
+                        </div>
+                        <button
+                            formaction="?page=table&table=<?= $id_game ?>&type=addEquipment"
+                            type="submit" class="btn btn-primary">Ajouter</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
