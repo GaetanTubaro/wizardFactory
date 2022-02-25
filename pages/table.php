d80e8c68d2bdc4233de351a827b68f1f92985b29
Yannis BOUGOURZI <kaiva.kono@gmail.com>
2022-02-25 10:23:39 +0100

    affichage fonctionnel

--- pages/table.php
+++ pages/table.php
@@ -40,23 +40,41 @@ $unequiped = $equipTable->fetchAll(PDO::FETCH_CLASS, Equipment::class);
 if ($unequiped) {
     foreach ($unequiped as $unequip) {
         ?>
-    <div class="d-flex flex-column align-items-center" style="height:15rem; width:100%;box-sizing:border-box">
-        <p><?=$unequip->getName()?>
-        </p>
-    </div>
-    <div class="card-footer d-flex justify-content-center">
-        <a href="?page=details&equipment=<?= $unequip->getId() ?>&type=deleteChar"
-            class="w-100">
-            <button class="btn m-0 p-0 w-100">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash"
-                    viewBox="0 0 16 16">
-                    <path
-                        d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-                    <path fill-rule="evenodd"
-                        d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
-                </svg>
-            </button>
-        </a>
+    <div class="card mb-3" style="width: 100%;">
+        <div class="card-body">
+            <div class="card-title d-flex align-items-center">
+                <h5 class="m-0 d-inline"><?= $unequip->getName() ?>
+                </h5>
+                <span class="ms-auto">
+                    <button class="btn m-1 p-1" data-bs-toggle="modal"
+                        data-bs-target="#equipmentChangeForm<?= $unequip->getId() ?>">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-pencil-square" viewBox="0 0 16 16">
+                            <path
+                                d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                            <path fill-rule="evenodd"
+                                d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                        </svg>
+                    </button>
+                    <a
+                        href="?page=details&character=<?= $_GET['character'] ?>&type=deleteEquipment&idEquipment=<?= $unequip->getId() ?>"><button
+                            class="btn m-1 p-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-trash" viewBox="0 0 16 16">
+                                <path
+                                    d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                <path fill-rule="evenodd"
+                                    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                            </svg>
+                        </button>
+                    </a>
+                </span>
+            </div>
+            <p class="card-text mb-1"><span style='font-style:bold'>Dégâts :</span> <?= $unequip->getDamages() ?>
+            </p>
+            <p class="card-text mb-1"><span style='font-style:bold'>Portée :</span> <?= $unequip->getRange() ?>
+            </p>
+        </div>
     </div>
 </div>
 <?php
