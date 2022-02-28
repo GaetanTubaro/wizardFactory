130267a6afab7ac164b274f16e2c073dfb1b9b06
Clara <clara.louise.goncalves@gmail.com>
2022-02-28 16:45:01 +0100

    Ajout de la modale disparue

--- pages/table.php
+++ pages/table.php
@@ -457,3 +457,26 @@ if (isset($_GET['type'])) {
         </div>
     </div>
 </div>
+
+<!-- ----------------------------------------------------------------------------------------------------------- -->
+<!----------------------------------------- Modal Change Name Table Start ----------------------------------------->
+<!-- ----------------------------------------------------------------------------------------------------------- -->
+<div class="modal fade" id="changeTableName" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">Changer le nom de cette partie</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form method="POST">
+                    <input type="text" class="form-control mb-3" name="chgname" required>
+                    <button formaction="?page=table&table=<?= $id_game ?>&type=changeName" type="submit" class="btn btn-primary">Modifier</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ----------------------------------------------------------------------------------------------------------- -->
+<!----------------------------------------- Modal Change Name Table End ------------------------------------------->
+<!-- ----------------------------------------------------------------------------------------------------------- --> 
